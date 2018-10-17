6076e3d0813ae1f55b772574aeb81640975cfd90
Stefan Larsen <33205551+bloody2k@users.noreply.github.com>
2018-10-17 21:53:04 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,12 +3,14 @@
 FIRST_RUN_CHECK="NOT_FIRST_RUN"
 if [ ! -e $FIRST_RUN_CHECK ]; then
     touch $FIRST_RUN_CHECK
-    echo "-- First container startup --"
+    echo "-- First start - Installing Monitor --"
     # YOUR_JUST_ONCE_LOGIC_HERE
     cd /
     git clone git://github.com/andrewjfreyer/monitor
     chmod +x /monitor/monitor.sh
-    bash /monitor/monitor.sh
+    echo "-- Starting Monitor --"
+    ( echo n ) | bash /monitor/monitor.sh
 else
-    echo "-- Not first container startup --"
+    echo "-- Starting Monitor --"
+    ( echo n ) | bash /monitor/monitor.sh
 fi
