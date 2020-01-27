4aa9cb889565d39338fa6ecd339431182ff4dca7
Stefan Larsen <33205551+bloody2k@users.noreply.github.com>
2020-01-27 16:00:10 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,17 +2,20 @@
 
 FIRST_RUN_CHECK="NOT_FIRST_RUN"
 if [ ! -e $FIRST_RUN_CHECK ]; then
+    touch $FIRST_RUN_CHECK    
     echo "-- First start - Installing Monitor --"
     # YOUR_JUST_ONCE_LOGIC_HERE
-    cd /
-    git clone git://github.com/andrewjfreyer/monitor
+    git clone git://github.com/andrewjfreyer/monitor /monitor
     chmod +x /monitor/monitor.sh
-    touch $FIRST_RUN_CHECK
-    echo "-- Starting Monitor --"
-    cd monitor
-    ( echo n ) | ./monitor.sh $MON_OPT
-else
-    echo "-- Starting Monitor --"
-    cd monitor
-    ( echo n ) | ./monitor.sh $MON_OPT
 fi
+
+#set -e
+
+# inspired from https://github.com/moby/moby/issues/16208#issuecomment-161770118
+service dbus start
+service bluetooth start
+hciconfig hci0 up
+
+echo "-- Starting Monitor --"
+cd monitor
+( echo n ) | ./monitor.sh $MON_OPT -D /config
