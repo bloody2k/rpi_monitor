043896b7af940f8aa7461add5f7a54b93db4bfd5
Stefan Larsen <33205551+bloody2k@users.noreply.github.com>
2020-01-28 12:26:33 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,7 +5,8 @@ if [ ! -e $FIRST_RUN_CHECK ]; then
     touch $FIRST_RUN_CHECK    
     echo "-- First start - Installing Monitor --"
     # YOUR_JUST_ONCE_LOGIC_HERE
-    git clone git://github.com/andrewjfreyer/monitor /monitor
+    cd /monitor
+    git clone git://github.com/andrewjfreyer/monitor
     chmod +x /monitor/monitor.sh
 fi
 
