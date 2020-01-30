008bf1f0221a201546ea4997418a25b746b730ed
Stefan Larsen <33205551+bloody2k@users.noreply.github.com>
2020-01-30 15:08:51 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
+# ==============================================================================
+# Limych's Hass.io Add-ons: Bluetooth Presence Monitor
+# Run Bluetooth Presence Monitor
+# ==============================================================================
 
-FIRST_RUN_CHECK="NOT_FIRST_RUN"
-if [ ! -e $FIRST_RUN_CHECK ]; then
-    touch $FIRST_RUN_CHECK    
-    echo "-- First start - Installing Monitor --"
-    # YOUR_JUST_ONCE_LOGIC_HERE
-    cd /monitor
-    git clone git://github.com/andrewjfreyer/monitor
-    chmod +x /monitor/monitor.sh
-fi
+echo "Starting required services..."
 
-# inspired from https://github.com/moby/moby/issues/16208#issuecomment-161770118
+cleanup() {
+    echo "Finishing services..."
+
+    service bluetooth stop
+    service dbus stop
+
+    exec echo
+}
+
+trap "cleanup" EXIT INT TERM
+
 service dbus start
 service bluetooth start
-hciconfig hci0 up
 
-#write out the timestamp of the last msg received
+echo "Updating Bluetooth Presence Monitor to latest version..."
+
+git fetch --depth=1
+git checkout origin/master -f
+git pull -f
+
+echo "Starting Bluetooth Presence Monitor..."
+
+# write out the timestamp of the last msg received
 date +%s > last_msg
 while true; do [[ -e main_pipe ]] && read line < main_pipe && date +%s > last_msg; done &
 
-echo "-- Starting Monitor --"
-cd /monitor
-( echo n ) | ./monitor.sh $MON_OPT -D /config
+while monitor -D /opt/monitor $MON_OPT >&2; do
+    echo "Restarting Bluetooth Presence Monitor..."
+done
+exit $?
